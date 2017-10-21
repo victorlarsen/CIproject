b51879e59c262004553041eb982dcd6728f7a70b
Victor Larsen <victorlarsen84@hotmail.com>
2017-10-21 23:35:16 +0200

    The second commit

---
+++ src/com/victorlarsen/Car.java
@@ -0,0 +1,31 @@
+package com.victorlarsen;
+
+/**
+ * Created by victor on 2017-09-08.
+ */
+
+//public talar om att vi har unrestricted access till klassen, vi har även private och protected
+//inkapsulering görs med private, public eller private
+//f = field, m = method, p = parameter. Visar även om de är privata etc.
+public class Car {
+
+    //Vi har nu definerat state med hjälp av fields.
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+// Objekt ska kunna få sin data via metoder och inte direkt (dvs fälten görs private). Detta möjliggör att vi kan validera datat innan den definieras för ett visst objekt.
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
---
+++ src/com/victorlarsen/Main.java
@@ -0,0 +1,14 @@
+package com.victorlarsen;
+
+
+//Definiera en klass i JAVA görs med public class sedan namnet på klassen.
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("carrera");
+        System.out.println("Model is " + porsche.getModel());
+    }
+}
